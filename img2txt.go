d48aac6ce23f3d6878050e1c921da7fa8ff344b0
Kazuhiko Hiramatsu <kazuhiko.hrmt@gmail.com>
2017-05-24 13:01:59 +0900

    Create img2txt.go

---
+++ img2txt.go
@@ -0,0 +1,28 @@
+package main
+
+import {
+        "fmt"
+        "image"
+        _ "image/png"
+        "log"
+        "os"
+}
+
+func main(){
+                file, err := os.Open("sample.png")
+                defer file.Close()
+
+                if err != nil {
+                        log.Fatal(err)
+                        return
+                }
+
+                config, _, err := image.DecodeConfig(file)
+                if err != nil {
+                        log.Fatal(err)
+                        return
+                }
+
+                fmt.Printf("(w,h)=(%d,%d)", config.Width, config.Height)
+
+}
