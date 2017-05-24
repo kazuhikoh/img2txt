c1b43bf2c931fcd0bf4723e5e0f9baeef6813dfe
kazuhikoh <kazuhiko.hrmt@gmail.com>
2017-05-24 21:58:09 +0900

    Fix Indent

--- img2txt.go
+++ img2txt.go
@@ -1,28 +1,29 @@
 package main
 
 import {
-        "fmt"
-        "image"
-        _ "image/png"
-        "log"
-        "os"
+	"fmt"
+	"image"
+	_ "image/png"
+	"log"
+	"os"
 }
 
 func main(){
-                file, err := os.Open("sample.png")
-                defer file.Close()
 
-                if err != nil {
-                        log.Fatal(err)
-                        return
-                }
+	file, err := os.Open("sample.png")
+	defer file.Close()
+	
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	
+	config, _, err := image.DecodeConfig(file)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
-                config, _, err := image.DecodeConfig(file)
-                if err != nil {
-                        log.Fatal(err)
-                        return
-                }
-
-                fmt.Printf("(w,h)=(%d,%d)", config.Width, config.Height)
+	fmt.Printf("(w,h)=(%d,%d)", config.Width, config.Height)
 
 }
