ceccfac29d163e919bbc6f9c314c65b9a66928ef
kazuhikoh <kazuhiko.hrmt@gmail.com>
2017-05-25 21:06:23 +0900

    Fix syntax error

--- img2txt.go
+++ img2txt.go
@@ -1,27 +1,29 @@
 package main
 
-import {
+import (
 	"fmt"
 	"image"
 	_ "image/png"
 	"log"
 	"os"
-}
+)
 
 func main(){
 
-	file, err := os.Open("sample.png")
+	if len(os.Args) != 2 {
+		log.Fatalf("Usage: %s filepath", os.Args[0])
+	}
+
+	file, err := os.Open(os.Args[1])
 	defer file.Close()
 	
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 	
 	config, _, err := image.DecodeConfig(file)
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 
 	fmt.Printf("(w,h)=(%d,%d)", config.Width, config.Height)
