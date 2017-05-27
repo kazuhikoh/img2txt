afceb8ba5ebf99535eee760970b37bd57582e00f
kazuhikoh <kazuhiko.hrmt@gmail.com>
2017-05-27 13:07:57 +0900

    Resolve getting color(RGBA) form image file

--- img2txt.go
+++ img2txt.go
@@ -3,29 +3,39 @@ package main
 import (
 	"fmt"
 	"image"
+	_ "image/jpeg"
 	_ "image/png"
 	"log"
 	"os"
 )
 
 func main(){
-
 	if len(os.Args) != 2 {
 		log.Fatalf("Usage: %s filepath", os.Args[0])
 	}
 
-	file, err := os.Open(os.Args[1])
-	defer file.Close()
-	
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	config, _, err := image.DecodeConfig(file)
-	if err != nil {
-		log.Fatal(err)
-	}
+	conf := func() (image.Config) {
+		file, err := os.Open(os.Args[1])
+		defer file.Close()
+		if err != nil { log.Fatal(err) }
+		
+		c, _, err := image.DecodeConfig(file)
+		if err != nil { log.Fatal(err) }
 
-	fmt.Printf("(w,h)=(%d,%d)", config.Width, config.Height)
+		return c
+	}()
 
+	img := func() (image.Image) {
+		file, err := os.Open(os.Args[1])
+		defer file.Close()
+		if err != nil { log.Fatal(err) }
+
+		i, _, err := image.Decode(file)
+		if err != nil { log.Fatal(err) }
+
+		return i
+	}()
+
+	fmt.Println(img.At(0,0))
+	fmt.Printf("(w,h)=(%d,%d)\n", conf.Width, conf.Height)
 }
