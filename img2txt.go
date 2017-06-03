67fcf46733611736ba92d0c3e12b382932d4ecd6
kazuhikoh <kazuhiko.hrmt@gmail.com>
2017-06-03 13:43:56 +0900

    gofmt

--- img2txt.go
+++ img2txt.go
@@ -9,33 +9,41 @@ import (
 	"os"
 )
 
-func main(){
+func main() {
 	if len(os.Args) != 2 {
 		log.Fatalf("Usage: %s filepath", os.Args[0])
 	}
 
-	conf := func() (image.Config) {
+	conf := func() image.Config {
 		file, err := os.Open(os.Args[1])
 		defer file.Close()
-		if err != nil { log.Fatal(err) }
-		
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		c, _, err := image.DecodeConfig(file)
-		if err != nil { log.Fatal(err) }
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		return c
 	}()
 
-	img := func() (image.Image) {
+	img := func() image.Image {
 		file, err := os.Open(os.Args[1])
 		defer file.Close()
-		if err != nil { log.Fatal(err) }
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		i, _, err := image.Decode(file)
-		if err != nil { log.Fatal(err) }
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		return i
 	}()
 
-	fmt.Println(img.At(0,0))
+	fmt.Println(img.At(0, 0))
 	fmt.Printf("(w,h)=(%d,%d)\n", conf.Width, conf.Height)
 }
